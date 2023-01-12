6a7de07da07e1e333214a34375870ec1bb368a70
MiuraKeiya <miurakeiya@yahoo.co.jp>
2023-01-12 23:09:28 +0900

    バリデーション実装

--- resources/ts/index.tsx
+++ resources/ts/index.tsx
@@ -2,6 +2,7 @@ import { createRoot } from 'react-dom/client';
 import { App} from './App';
 import { BrowserRouter, Routes, Route} from "react-router-dom";
 import { Profile } from './components/Profile';
+import { Home } from './components/Home';
 
 const container = document.getElementById('app');
 const root = createRoot(container!); // createRoot(container!) if you use TypeScript
@@ -10,8 +11,9 @@ root.render(
 // <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<App />} />
+        <Route path="/" element={<App />} />
         <Route path="/Profile" element={<Profile />} />
+        <Route path="/Home" element={<Home />} />
       </Routes>
     </BrowserRouter>
 // </React.StrictMode>,
