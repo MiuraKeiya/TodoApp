e9ac9d5b9316547736c63b9a0e319b239a8f8393
MiuraKeiya <miurakeiya@yahoo.co.jp>
2023-02-22 01:18:43 +0900

    ユーザーテーブルカラム削除

--- database/migrations/2023_02_22_011038_modify_users.php
+++ database/migrations/2023_02_22_011038_modify_users.php
@@ -13,12 +13,8 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('tasks', function (Blueprint $table) {
-            $table->id();
-            $table->string('title');
-            $table->text('body');
-            $table->foreignId('user_id')->constrained();
-            $table->timestamps();
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('email_verified_at');
         });
     }
 
@@ -29,6 +25,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('tasks');
+        //
     }
 };
